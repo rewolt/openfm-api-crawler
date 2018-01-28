e3735e954cec921de9567d7d95e54c564c5e4294
Bartłomiej Tomczak <rewolt90@gmail.com>
2018-01-28 22:18:35 +0100

    Add JsonDownloader class

---
+++ Open.fm API Crawler/JsonDownloader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+
+namespace OpenFM_API_Crawler
+{
+    class JsonDownloader
+    {
+        public Uri Url { get; set; }
+
+        public JsonDownloader(Uri apiUrl)
+        {
+            Url = apiUrl;
+        }
+
+        public string GetJson()
+        {
+            var client = new RestClient();
+
+            client.UserAgent = "Mozilla /5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36";
+            client.AddDefaultHeader("Accept - Encoding", "gzip, deflate");
+            client.AddDefaultHeader("Accept-Language", "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7");
+
+            var restRequest = new RestRequest(Url);
+            var response = client.Execute(restRequest);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return response.Content;
+            else
+                return string.Empty;
+            
+        }
+    }
+}
--- Open.fm API Crawler/Program.cs
+++ Open.fm API Crawler/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace Open.fm_API_Crawler
+namespace OpenFM_API_Crawler
 {
     class Program
     {
+        private static string _apiUrl => "http://open.fm/api/api-ext/v2/channels/long.json?_=1517168611.79";
+
+        //// Browser header to API
+        //GET /api/api-ext/v2/channels/long.json? _ = 1517168611.79 HTTP/1.1
+        // Host: open.fm
+        // Connection: keep-alive
+        // Accept: */*
+        // X-Requested-With: XMLHttpRequest
+        // User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36
+        // Referer: http://open.fm/play/82
+        // Accept-Encoding: gzip, deflate
+        // Accept-Language: pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7
+        // Cookie: PWA_adbd=1; pvid=f29de175c69358e6ce40
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (Uri.IsWellFormedUriString(_apiUrl, UriKind.Absolute))
+                Console.WriteLine("SUper uri");
+            var uri = new Uri(_apiUrl);
+
+            var downloader = new JsonDownloader(uri);
+            Console.WriteLine(downloader.GetJson());
+            Console.ReadKey();
         }
     }
 }
